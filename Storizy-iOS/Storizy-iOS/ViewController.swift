9e1493e5d9114989b7ee3b137427bf1439b0c420
sujeong lim <20180998@sungshin.ac.kr>
2021-11-03 23:06:12 +0900

    INIT

---
+++ Storizy-iOS/Storizy-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storizy-iOS
+//
+//  Created by 임수정 on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
