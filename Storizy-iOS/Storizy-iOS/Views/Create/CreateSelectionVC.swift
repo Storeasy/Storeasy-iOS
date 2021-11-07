19924faffd5bf0f2a32fd543fc98566c5d35a643
sujeong lim <20180998@sungshin.ac.kr>
2021-11-07 18:18:46 +0900

    Feat: 글작성 탭 메인 화면 구현 #8

---
+++ Storizy-iOS/Storizy-iOS/Views/Create/CreateSelectionVC.swift
@@ -0,0 +1,25 @@
+//
+//  CreateSelectionVC.swift
+//  Storizy-iOS
+//
+//  Created by 임수정 on 2021/11/07.
+//
+
+import UIKit
+
+class CreateSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // 프로젝트 생성 버튼 클릭
+    @IBAction func projectCreateSelectAction(_ sender: Any) {
+    }
+    
+    // 페이지 생성 버튼 클릭
+    @IBAction func pageCreateSelectAction(_ sender: Any) {
+    }
+}
