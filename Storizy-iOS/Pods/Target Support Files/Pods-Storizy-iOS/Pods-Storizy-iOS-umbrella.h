40828b0568a58b6bdab8bb06e966872718f59e57
sujeong lim <20180998@sungshin.ac.kr>
2021-11-11 01:46:45 +0900

    Feat: alamofire 설치 #12

---
+++ Storizy-iOS/Pods/Target Support Files/Pods-Storizy-iOS/Pods-Storizy-iOS-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Storizy_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Storizy_iOSVersionString[];
+
