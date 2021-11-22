b53b4f1762543a795afb56a614b0a9af4251d59b
sujeong lim <20180998@sungshin.ac.kr>
2021-11-22 15:22:34 +0900

    Feat: 페이지 수정 UI 적용 #7

--- Storizy-iOS/Storizy-iOS/Views/Page/EditPageVC.swift
+++ Storizy-iOS/Storizy-iOS/Views/Page/EditPageVC.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class EditPageVC: UIViewController {
 
-    @IBOutlet weak var imageCollectionView: UICollectionView!
+    @IBOutlet weak var headBarView: UIView!
+    @IBOutlet weak var projectTitleTF: UITextField!
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var tagCV: UICollectionView!
+    @IBOutlet weak var contentTV: UITextView!
+    @IBOutlet weak var imageCV: UICollectionView!
+    @IBOutlet weak var doneBTN: UIButton!
+    
+    var tags: [String] = ["러시아워","유기현최고"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        registerNib()
         
     }
     
@@ -26,19 +36,68 @@ class EditPageVC: UIViewController {
         self.navigationController?.popViewController(animated: true)
 
     }
+    
+    // MARK: - Nib
+    func registerNib(){
+        let storyTagNibName = UINib(nibName: "StoryTagCell", bundle: nil)
+        tagCV.register(storyTagNibName, forCellWithReuseIdentifier: "StoryTagCell")
+    }
+    
+    // MARK: - UI
+    func frameUI<T: UIView>(_ view: T){
+        view.layer.cornerRadius = 12
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor(named: "light_gray2")?.cgColor
+        view.clipsToBounds = true
+    }
+    
+    func setUI(){
+        // 헤더 그림자
+        headBarView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        headBarView.layer.shadowRadius = 6
+        headBarView.layer.shadowOpacity = 1
+        headBarView.layer.shadowColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.1).cgColor
+        // 입력 칸 테두리
+        frameUI(projectTitleTF)
+        frameUI(titleTF)
+        // 내용 칸
+        contentTV.layer.cornerRadius = 12
+        contentTV.textContainerInset = UIEdgeInsets(top: 12, left: 6,bottom: 12,right: 12)
+        // 완료 버튼
+        doneBTN.layer.cornerRadius = 12
+    }
+    
 
 }
 
 // 이미지 collection view
 extension EditPageVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        // 이미지
+        if collectionView == imageCV {
+            return 5
+        }
+        // 태그
+        else {
+            return tags.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imgCell", for: indexPath) as! ImgCell
-        // 이미지 등록
-        return cell
+        // 이미지
+        if collectionView == imageCV {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImgCell", for: indexPath) as! ImgCell
+            cell.imageView.layer.cornerRadius = 10
+            return cell
+        }
+        // 태그
+        else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StoryTagCell", for: indexPath) as! StoryTagCell
+            cell.tagNameLB.text = tags[indexPath.item]
+            cell.frameView.backgroundColor = UIColor(named: "tag_yellow-light")
+            cell.tagNameLB.textColor = UIColor(named: "tag_yellow")
+            return cell
+        }
     }
     
     
